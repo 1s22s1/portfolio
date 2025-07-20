12b239784247194674154cf4b5fac326f5c907eb
1s22s1 <1s22s1@users.noreply.github.com>
2025-07-20 12:46:25 +0900

    Add qualification page

---
+++ app/qualification.tsx
@@ -0,0 +1,31 @@
+const Qualification = () => {
+    const qualifications = [
+        { yearMonth: "2025/07", name: "実用数学技能検定準2級" },
+        { yearMonth: "2025/07", name: "ビジネス実務法務検定3級" },
+        { yearMonth: "2025/06", name: "丙種危険物取扱者" },
+        { yearMonth: "2025/06", name: "ファイナンシャルプランナー2級" },
+        { yearMonth: "2025/05", name: "簿記2級" },
+        { yearMonth: "2024/12", name: "情報処理安全確保支援士試験" },
+        { yearMonth: "2024/11", name: "ファイナンシャルプランナー3級" },
+        { yearMonth: "2024/10", name: "AWS DVA" },
+        { yearMonth: "2024/09", name: "簿記3級" },
+        { yearMonth: "2024/09", name: "G検定" },
+        { yearMonth: "2024/08", name: "AWS CLF" },
+        { yearMonth: "2024/07", name: "データサイエンティスト検定" },
+        { yearMonth: "2024/07", name: "統計検定2級" },
+        { yearMonth: "2024/07", name: "TOEIC 755" },
+        { yearMonth: "2024/07", name: "応用情報" },
+        { yearMonth: "2024/02", name: "統計検定3級" },
+        { yearMonth: "2024/02", name: "基本情報" },
+    ]
+
+    return (
+        <div className="flex items-center justify-center pt-16 pb-4">
+            <ul>
+                {qualifications.map((e, i) => <li key={i}>{e.yearMonth} {e.name}</li>)}
+            </ul>
+        </div>
+    )
+}
+
+export default Qualification;
--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -4,9 +4,8 @@ export function Welcome() {
   return (
     <div className="flex items-center justify-center pt-16 pb-4">
       <ul>
-        <li>
-          <Link to="/oss">Oss</Link>
-        </li>
+        <li><Link to="/qualification">保有資格</Link></li>
+        <li><Link to="/oss">OSS</Link></li>
       </ul>
     </div>
   )
