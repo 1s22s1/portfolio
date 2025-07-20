fb81ad5387c095a699ae634ffe18d88fe0e07cc5
1s22s1 <1s22s1@users.noreply.github.com>
2025-07-20 12:41:15 +0900

    :fire: Remove /

--- app/oss.tsx
+++ app/oss.tsx
@@ -1,7 +1,7 @@
 const Oss = () => {
     const ossArr = [
         { href: "https://github.com/terrastruct/d2/issues/507", title: "n is lack in svg" },
-        { href: "https://github.com/ruby/csv/issues/237", title: "CSV::Table#headers returns[:\\\"\\\", :\\\"\\\"] when header is Japanese" },
+        { href: "https://github.com/ruby/csv/issues/237", title: "CSV::Table#headers returns[:\"\", :\"\"] when header is Japanese" },
         { href: "https://github.com/JuliaLang/julia/issues/56208", title: "Using length() in the conditional expression for while is slow" },
         { href: "https://github.com/microsoft/vscode/issues/231562", title: "terminal.integrated.enableMultiLinePasteWarning is setted to false" },
         { href: "https://github.com/ruby/rss/pull/59", title: "Simplify sample code" },
