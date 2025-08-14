21a0557215bb3dde1347bb109cc6a9b5bef909a4
1s22s1 <1s22s1@users.noreply.github.com>
2025-08-14 17:51:12 +0900

    Add shougi

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -5,7 +5,7 @@ export function Welcome() {
     <div className="flex items-center justify-center pt-16 pb-4">
       <ul>
         <li><Link to="/self_introduction">自己紹介</Link></li>
-        <li><Link to="/qualification">保有資格</Link></li>
+        <li><Link to="/qualification">保有資格等</Link></li>
         <li><Link to="/oss">OSS</Link></li>
         <li><Link to="/lucky_uuid">ラッキーUUID</Link></li>
         <li><Link to="/lucky_color">ラッキーカラー</Link></li>
