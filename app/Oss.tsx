d8d63fa6064b6f1fcfea1d678e28b479ddfebf93
1s22s1 <1s22s1@users.noreply.github.com>
2025-08-22 15:55:22 +0900

    Update OSS links

--- app/Oss.tsx
+++ app/Oss.tsx
@@ -4,8 +4,6 @@ const Oss = () => {
         { href: "https://github.com/ruby/csv/issues/237", title: "CSV::Table#headers returns[:\"\", :\"\"] when header is Japanese" },
         { href: "https://github.com/JuliaLang/julia/issues/56208", title: "Using length() in the conditional expression for while is slow" },
         { href: "https://github.com/microsoft/vscode/issues/231562", title: "terminal.integrated.enableMultiLinePasteWarning is setted to false" },
-        { href: "https://github.com/ruby/rss/pull/59", title: "Simplify sample code" },
-        { href: "https://github.com/gka/chroma.js/pull/372", title: "Fix link on chroma.random" },
     ];
 
     return (
